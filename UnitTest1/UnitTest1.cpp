ca5ef0593d08361d9bf7ee756e93bfb2b892e0a6
Yura <urijkisil2007@gmail.com>
2024-10-27 20:24:16 +0200

    first master

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab_8_2/lab_8_2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+        TEST_METHOD(TestCountAInLastWord)
+        {
+            string lastWord;
+
+            // Тест 1: Звичайний випадок
+            string input1 = "Hello world apple";
+            size_t result1 = CountAInLastWord(input1, lastWord);
+            Assert::AreEqual((size_t)1, result1);
+            Assert::AreEqual(string("apple"), lastWord);
+
+            // Тест 2: Без літер 'a'
+            string input2 = "Hello world";
+            size_t result2 = CountAInLastWord(input2, lastWord);
+            Assert::AreEqual((size_t)0, result2);
+            Assert::AreEqual(string("world"), lastWord);
+
+            // Тест 3: Лише пробіли
+            string input3 = "     ";
+            size_t result3 = CountAInLastWord(input3, lastWord);
+            Assert::AreEqual((size_t)0, result3);
+            Assert::AreEqual(string(""), lastWord); // останнє слово пусте
+
+            // Тест 4: Різні регістри
+            string input4 = "A apple a";
+            size_t result4 = CountAInLastWord(input4, lastWord);
+            Assert::AreEqual((size_t)1, result4);
+            Assert::AreEqual(string("a"), lastWord);
+        }
+
+	};
+}
---
+++ lab_8_2/lab_8_2.cpp
@@ -0,0 +1,33 @@
+﻿// lab_8_2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+size_t CountAInLastWord(const string s, string& lastWord)
+{
+	size_t lastSpace = s.find_last_not_of(" "); // останній символ, що не є пробілом
+	size_t wordStart = s.find_last_of(" ", lastSpace); //пробіл і початко останнього слова
+	lastWord = s.substr(wordStart + 1, lastSpace - wordStart); //слово
+
+	int countA = 0;
+	for (int i = 0; i < lastWord.length(); i++) // довжина
+	{
+		if (lastWord[i] == 'a')
+			countA++;
+	}
+	return countA;
+}
+
+int main()
+{
+	string str;
+	cout << "Enter string:" << endl;
+	getline(cin, str);
+	string lastWord;
+
+	cout << "Number of 'a' letters in the last word: " << CountAInLastWord(str, lastWord) << " in the last word: " << lastWord << endl;
+
+	return 0;
+}
